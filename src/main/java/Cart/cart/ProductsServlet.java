897df8ba1e44d4f6ac6202805abd245daa5b3522
PhanQuocPhu <phanquocphu1998@gmail.com>
2021-10-25 23:12:25 +0700

    Change in productservlet

--- src/main/java/Cart/cart/ProductsServlet.java
+++ src/main/java/Cart/cart/ProductsServlet.java
@@ -1,7 +1,9 @@
 package Cart.cart;
 
-import Cart.business.Product;
+import Cart.entity.Product;
 import Cart.data.ProductIO;
+import Cart.model.ProductModel;
+import util.ServletUtil;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,7 +12,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet(name = "ProductsServlet", value = "/loadProducts")
 public class ProductsServlet extends HttpServlet {
@@ -20,13 +24,13 @@ public class ProductsServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = request.getSession();
-        String path = getServletContext().getRealPath("/WEB-INF/products.txt");
-        ArrayList<Product> products = ProductIO.getProducts(path);
-        session.setAttribute("products", products);
-
-        String url = "/Cart/index.jsp";
-        getServletContext()
-                .getRequestDispatcher(url)
-                .forward(request, response);
+        List<Product> listp = null;
+        try {
+            listp = ProductModel.getAll();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        session.setAttribute("products", listp);
+        ServletUtil.forward("/Cart/index.jsp", request, response);
     }
 }
